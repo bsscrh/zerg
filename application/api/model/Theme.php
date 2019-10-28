ccac88248d71a2d180dfec753bcf0dcc20fc6eb9
ronghuang <ronghuang@ciandt.com>
2019-10-28 15:14:42 +0800

    add the one to one relation.

--- application/api/model/Theme.php
+++ application/api/model/Theme.php
@@ -4,5 +4,11 @@ namespace app\api\model;
 
 class Theme extends BaseModel
 {
-    //
+    public function topicImg() {
+    	return $this->belongsTo('Image','topic_img_id','id');
+    }
+
+    public function topImg() {
+    	return $this->belongsTo('Image','head_img_id','id');
+    }
 }
