1c39d90f3cda68e61b629d81ae96825adee51219
bss <442484635@qq.com>
2019-11-03 19:25:46 +0800

    auto timestamp

--- application/api/model/Wxnews.php
+++ application/api/model/Wxnews.php
@@ -4,5 +4,8 @@ namespace app\api\model;
 
 class Wxnews extends BaseModel
 {
-    
+    protected $autoWriteTimestamp = true; //开启自动时间戳,默认对create_time和update_time生效
+    protected $createTime = 'create_time'; //创建时间字段
+    protected $updateTime = 'update_time'; //更新时间字段
+    protected $dateFormat = 'Y/m/d'; //时间字段取出后的默认时间格式
 }
