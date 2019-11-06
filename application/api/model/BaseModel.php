4897878bfce19b58770d6ef6abad47710f3c2401
bss <442484635@qq.com>
2019-11-06 21:14:20 +0800

    模型构造器初始化

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -7,13 +7,26 @@ use think\Request;
 
 class BaseModel extends Model
 {
+    protected $img_prefix;
+
+    //tp5.0模型初始化是initialize(),控制器初始化是_initialize()
+    //tp5.1控制器初始化是initialize()
+    //tp5.1模型初始化 protected static function init()，还未用过
+    public function initialize(){
+        $this->img_prefix = config('setting.img_prefix');
+    }
+
     protected function prefixImgUrl($value, $data) {
     	if($data['from'] == 1) {
-    		$img_prefix = config('setting.img_prefix');
-			$url = $img_prefix.$value;
+			$url = $this->img_prefix.$value;
 			return $url;
     	}
     	return $value;
+    }
+
+    protected function getMainImgUrl($value) {
+        $url = $this->img_prefix.$value;
+        return $url;
     }
 
     protected function articleImg($value,$flag){
--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -7,4 +7,8 @@ class Product extends BaseModel
     protected $hidden = [
         'delete_time', 'main_img_id', 'pivot', 'from', 'category_id',
         'create_time', 'update_time'];
+
+    public function getMainImgUrlAttr($value){
+    	return $this->getMainImgUrl($value);
+    }
 }
