92191beb5a6d4cd17d5e398ff5f0e60e9dad50de
unknown <442484635@qq.com>
2019-11-25 16:14:18 +0800

    9-3 构建code token验证体系 company

--- application/route.php
+++ application/route.php
@@ -1,22 +1,18 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 use think\Route;
 
 Route::get('api/:v/banner/:id', 'api/:v.Banner/getBanner');
+
 Route::get('api/:v/theme', 'api/:v.Theme/getSimpleList');
 Route::get('api/:v/theme/:id', 'api/:v.Theme/getComplexOne');
+
 Route::get('api/:v/category/all', 'api/:v.Category/getCategories');
+
 Route::get('api/:v/product/recent', 'api/:v.Product/getRecent');
 Route::get('api/:v/product/by_category', 'api/:v.Product/getAllInCategory');
+
+Route::post('api/:v/token/user', 'api/:v.Token/getToken');
 
 Route::get('api/:v/wxnews', 'api/:v.Wxnews/getNewsList');
 Route::get('api/:v/wxnews/:id', 'api/:v.Wxnews/getNewsDetail');
