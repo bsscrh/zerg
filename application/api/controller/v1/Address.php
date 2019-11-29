dc7ec27fc027efded3b376209f423333f541ef6c
unknown <442484635@qq.com>
2019-11-29 13:28:20 +0800

    9-14 用户地址新增和更新 company

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -2,13 +2,28 @@
 
 namespace app\api\controller\v1;
 
+use app\api\model\User;
 use app\api\validate\AddressNew;
 use app\api\service\Token;
+use app\lib\exception\SuccessMessage;
+use app\lib\exception\UserException;
 
 class Address
 {
     public function createOrUpdateAddress(){
         (new AddressNew())->goCheck();
         $uid = Token::getCurrentUid();
+        $user = User::get($uid);
+        if(!$user){
+            throw new UserException();
+        }
+
+        $userAddress = $user->address;
+        if(!$userAddress){
+            $user->address()->save();
+        }else{
+            $user->address->save();
+        }
+        return new SuccessMessage();
     }
 }
