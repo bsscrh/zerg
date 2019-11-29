8ec3c2429453ba4cf2199597c88680a6b6c99c45
unknown <442484635@qq.com>
2019-11-29 14:23:23 +0800

    9-15 用户地址参数过滤 company

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -11,18 +11,21 @@ use app\lib\exception\UserException;
 class Address
 {
     public function createOrUpdateAddress(){
-        (new AddressNew())->goCheck();
+        $validate = new AddressNew();
+        $validate->goCheck();
+
         $uid = Token::getCurrentUid();
         $user = User::get($uid);
         if(!$user){
             throw new UserException();
         }
-
+        //只保存validate规则里面验证的数据，防止恶意传值
+        $data = $validate->getDataByRule(input('post.'));
         $userAddress = $user->address;
         if(!$userAddress){
-            $user->address()->save();
+            $user->address()->save($data);
         }else{
-            $user->address->save();
+            $user->address->save($data);
         }
         return new SuccessMessage();
     }
